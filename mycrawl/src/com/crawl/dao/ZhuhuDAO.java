e4eac0b337d700a218708659852b86b48f628434
wycm <1057160387@qq.com>
2016-04-27 13:18:53 +0800

    update

---
+++ mycrawl/src/com/crawl/dao/ZhuhuDAO.java
@@ -0,0 +1,124 @@
+package com.crawl.dao;
+
+import com.crawl.entity.User;
+import com.crawl.util.MyLogger;
+import com.crawl.zhihu.user.ParseWebPageTask;
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by Administrator on 2016/4/27 0027.
+ */
+public class ZhuhuDAO {
+    private static Logger logger = MyLogger.getMyLogger(ZhuhuDAO.class);
+    /**
+     * 判断该数据库中是否存在该用户
+     * @param cn
+     * @param sql 判断该sql数据库中是否存在
+     * @return
+     */
+    public synchronized static boolean isContain(Connection cn, String sql) throws SQLException {
+        int num;
+        PreparedStatement pstmt;
+        pstmt = cn.prepareStatement(sql);
+        ResultSet rs = pstmt.executeQuery();
+        while(rs.next()){
+            num = rs.getInt("count(*)");
+            if(num == 0){
+                return false;
+            }else{
+                return true;
+            }
+        }
+        rs.close();
+        pstmt.close();
+        return true;
+    }
+    /**
+     * 将用户插入数据库
+     * @param cn
+     * @param u
+     * @throws SQLException
+     */
+    public synchronized static boolean insetToDB(Connection cn,User u) throws SQLException {
+        String isContainSql = "select count(*) from user WHERE url='" + u.getUrl() + "'";
+        if(isContain(cn,isContainSql)){
+//			cn.close();
+            logger.info("数据库已经存在该用户---" + u.getUrl() + "---当前已获取用户数量为:" + ParseWebPageTask.userCount);
+            return false;
+        }
+        String colum = "location,business,sex,employment,username,url,agrees,thanks,asks," +
+                "answers,posts,followees,followers,hashId,education";
+        String values = "?,?,?,?,?,?,?,?,?,?,?,?,?,?,?";
+        String sql = "insert into user (" + colum + ") values(" +values+")";
+        PreparedStatement pstmt;
+        pstmt = cn.prepareStatement(sql);
+        pstmt.setString(1,u.getLocation());
+        pstmt.setString(2,u.getBusiness());
+        pstmt.setString(3,u.getSex());
+        pstmt.setString(4,u.getEmployment());
+        pstmt.setString(5,u.getUsername());
+        pstmt.setString(6,u.getUrl());
+        pstmt.setInt(7,u.getAgrees());
+        pstmt.setInt(8,u.getThanks());
+        pstmt.setInt(9,u.getAsks());
+        pstmt.setInt(10,u.getAnswers());
+        pstmt.setInt(11,u.getPosts());
+        pstmt.setInt(12,u.getFollowees());
+        pstmt.setInt(13,u.getFollowers());
+        pstmt.setString(14,u.getHashId());
+        pstmt.setString(15,u.getEducation());
+        pstmt.executeUpdate();
+        pstmt.close();
+//		cn.close();
+        u = null;
+        ParseWebPageTask.userCount++;
+        logger.info("插入用户成功---已获取" + ParseWebPageTask.userCount + "用户");
+        return true;
+    }
+
+    /**
+     * 将访问过的链接插入数据库
+     * @param cn 数据库连接
+     * @param md5Href 经过md5处理后的链接
+     * @return
+     * @throws SQLException
+     */
+    public synchronized static boolean insertHref(Connection cn,String md5Href) throws SQLException {
+        String isContainSql = "select count(*) from href WHERE href='" + md5Href + "'";
+        if(isContain(cn,isContainSql)){
+            logger.info("数据库已经存在该链接---" + md5Href);
+            return false;
+        }
+        String sql = "insert into href (href) values( ?)";
+        PreparedStatement pstmt;
+        pstmt = cn.prepareStatement(sql);
+        pstmt.setString(1,md5Href);
+        pstmt.executeUpdate();
+        pstmt.close();
+        logger.info("链接插入成功---");
+        return true;
+    }
+
+    /**
+     * 清空表
+     * @param cn
+     * @throws SQLException
+     */
+    public synchronized static void deleteHrefTable(Connection cn){
+        String sql = "DELETE FROM href";
+        PreparedStatement pstmt = null;
+        try {
+            pstmt = cn.prepareStatement(sql);
+            pstmt.executeUpdate();
+            pstmt.close();
+            logger.info("href表删除成功---");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- mycrawl/src/com/crawl/zhihu/user/ParseWebPageTask.java
+++ mycrawl/src/com/crawl/zhihu/user/ParseWebPageTask.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.util.concurrent.ThreadPoolExecutor;
 
 import com.crawl.dao.ConnectionManage;
+import com.crawl.dao.ZhuhuDAO;
 import com.crawl.entity.User;
 import com.crawl.util.Md5Util;
 import com.crawl.util.MyLogger;
@@ -55,7 +56,7 @@ public class ParseWebPageTask implements Runnable{
 			if(doc.select("title").size() != 0){
 				//解析用户信息(包含html标签，为用户页面)
 				u = parseUserdetail(doc);
-				if(insetToDB(cn,u)){
+				if(ZhuhuDAO.insetToDB(cn,u)){
 //					storage.getResult().getUserVector().add(u);//将用户存入Vector
 				}
 				for(int i = 0;i < u.getFollowees()/20 + 1;i++){
@@ -98,7 +99,7 @@ public class ParseWebPageTask implements Runnable{
 //			if(storage.getResult().getHrefSet().size() >= 10000){
 //				storage.getResult().getHrefSet().clear();
 //			}
-		if(insertHref(cn,md5Href) || gwpThreadPool.getQueue().size() <= 50){
+		if(ZhuhuDAO.insertHref(cn,md5Href) || gwpThreadPool.getQueue().size() <= 50){
 //			该链接未访问过，将用户链接插入数据库或者当前获取任务线程池没有任务（防止出现死锁）
 			if(pwpThreadPool.getQueue().size() <= 100){
 				//解析线程池等待任务小于100时，才加入获取任务队列
@@ -117,7 +118,7 @@ public class ParseWebPageTask implements Runnable{
 		}
 	}
 	/**
-	 * 将关注页面的个人信息解析出来
+	 * 解析个人资料
 	 * @param doc
 	 * @return
      */
@@ -163,113 +164,7 @@ public class ParseWebPageTask implements Runnable{
 			return e.attr("title");
 		}
 	}
-	/**
-	 * 判断该数据库中是否存在该用户
-	 * @param cn
-	 * @param sql 判断该sql数据库中是否存在
-     * @return
-     */
-	public synchronized static boolean isContain(Connection cn,String sql) throws SQLException {
-		int num;
-		PreparedStatement pstmt;
-		pstmt = cn.prepareStatement(sql);
-		ResultSet rs = pstmt.executeQuery();
-		while(rs.next()){
-			num = rs.getInt("count(*)");
-			if(num == 0){
-				return false;
-			}else{
-				return true;
-			}
-		}
-		rs.close();
-		pstmt.close();
-		return true;
-	}
-	/**
-	 * 将用户插入数据库
-	 * @param cn
-	 * @param u
-	 * @throws SQLException
-     */
-	public synchronized static boolean insetToDB(Connection cn,User u) throws SQLException {
-		String isContainSql = "select count(*) from user WHERE url='" + u.getUrl() + "'";
-		if(isContain(cn,isContainSql)){
-//			cn.close();
-			logger.info("数据库已经存在该用户---" + u.getUrl() + "---当前已获取用户数量为:" + userCount);
-			return false;
-		}
-		String colum = "location,business,sex,employment,username,url,agrees,thanks,asks," +
-				"answers,posts,followees,followers,hashId,education";
-		String values = "?,?,?,?,?,?,?,?,?,?,?,?,?,?,?";
-		String sql = "insert into user (" + colum + ") values(" +values+")";
-		PreparedStatement pstmt;
-		pstmt = cn.prepareStatement(sql);
-		pstmt.setString(1,u.getLocation());
-		pstmt.setString(2,u.getBusiness());
-		pstmt.setString(3,u.getSex());
-		pstmt.setString(4,u.getEmployment());
-		pstmt.setString(5,u.getUsername());
-		pstmt.setString(6,u.getUrl());
-		pstmt.setInt(7,u.getAgrees());
-		pstmt.setInt(8,u.getThanks());
-		pstmt.setInt(9,u.getAsks());
-		pstmt.setInt(10,u.getAnswers());
-		pstmt.setInt(11,u.getPosts());
-		pstmt.setInt(12,u.getFollowees());
-		pstmt.setInt(13,u.getFollowers());
-		pstmt.setString(14,u.getHashId());
-		pstmt.setString(15,u.getEducation());
-		pstmt.executeUpdate();
-		pstmt.close();
-//		cn.close();
-		u = null;
-		userCount++;
-		logger.info("插入用户成功---已获取" + userCount + "用户");
-		return true;
-	}
 
-	/**
-	 * 将访问过的链接插入数据库
-	 * @param cn 数据库连接
-	 * @param md5Href 经过md5处理后的链接
-	 * @return
-	 * @throws SQLException
-     */
-	public synchronized static boolean insertHref(Connection cn,String md5Href) throws SQLException {
-		String isContainSql = "select count(*) from href WHERE href='" + md5Href + "'";
-		if(isContain(cn,isContainSql)){
-			logger.info("数据库已经存在该链接---" + md5Href);
-			return false;
-		}
-		String sql = "insert into href (href) values( ?)";
-		PreparedStatement pstmt;
-		pstmt = cn.prepareStatement(sql);
-		pstmt.setString(1,md5Href);
-		pstmt.executeUpdate();
-		pstmt.close();
-//		cn.close();
-		logger.info("链接插入成功---");
-		return true;
-	}
-
-	/**
-	 * 清空表
-	 * @param cn
-	 * @throws SQLException
-     */
-	public synchronized static void deleteHrefTable(Connection cn){
-		String sql = "DELETE FROM href";
-		PreparedStatement pstmt = null;
-		try {
-			pstmt = cn.prepareStatement(sql);
-			pstmt.executeUpdate();
-			pstmt.close();
-			logger.info("href表删除成功---");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
 	public static void main(String args []){
 		/*ParseWebPageTask p = new ParseWebPageTask();
 		String s = chcUtils.readFileByBytes("d:/test/llxs.txt");
