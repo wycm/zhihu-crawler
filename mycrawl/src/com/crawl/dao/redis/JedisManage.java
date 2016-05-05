ad04390ba112a6d24efc181cc7bd8cc910d76125
wycm <1057160387@qq.com>
2016-05-05 21:04:09 +0800

    add sql file

---
+++ mycrawl/src/com/crawl/dao/redis/JedisManage.java
@@ -0,0 +1,7 @@
+package com.crawl.dao.redis;
+
+/**
+ * Created by Administrator on 2016/5/4 0004.
+ */
+public class JedisManage {
+}
---
+++ mycrawl/zhihu.sql
@@ -0,0 +1,52 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : RemoteServer
+Source Server Version : 50537
+Source Host           : 120.27.36.59:3306
+Source Database       : zhihu
+
+Target Server Type    : MYSQL
+Target Server Version : 50537
+File Encoding         : 65001
+
+Date: 2016-05-05 20:27:47
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for href
+-- ----------------------------
+DROP TABLE IF EXISTS `href`;
+CREATE TABLE `href` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `href` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `href` (`href`(255))
+) ENGINE=InnoDB AUTO_INCREMENT=2227817 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `location` varchar(255) DEFAULT NULL,
+  `business` varchar(255) DEFAULT NULL,
+  `sex` varchar(255) DEFAULT NULL,
+  `employment` varchar(255) DEFAULT NULL,
+  `education` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `agrees` int(11) DEFAULT NULL,
+  `thanks` int(11) DEFAULT NULL,
+  `asks` int(11) DEFAULT NULL,
+  `answers` int(11) DEFAULT NULL,
+  `posts` int(11) DEFAULT NULL,
+  `followees` int(11) DEFAULT NULL,
+  `followers` int(11) DEFAULT NULL,
+  `hashId` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=976838 DEFAULT CHARSET=utf8;
