731d6fe403c9171646b0c8085f393585570e579f
wycm <1057160387@qq.com>
2016-05-06 20:37:03 +0800

    nothing

--- mycrawl/src/com/crawl/util/MyLogger.java
+++ mycrawl/src/com/crawl/util/MyLogger.java
@@ -5,25 +5,23 @@ import org.apache.log4j.PropertyConfigurator;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Properties;
-
-/**
- * Created by wycm on 2016/4/12 0012.
- */
 public class MyLogger extends Logger{
     protected MyLogger(String name) {
         super(name);
     }
     private static Properties setLogProperty(){
         Properties p = new Properties();
+        Properties ipP = new Properties();
         String ip = null;
         try {
             p.load(MyLogger.class.getResourceAsStream("/resources/log4j.properties"));
+            ipP.load(MyLogger.class.getResourceAsStream("/resources/server.properties"));
             InetAddress addr = InetAddress.getLocalHost();
             ip = addr.getHostAddress().toString(); //获取本机ip
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if(ip.equals("10.252.19.85")){
+        if(ip.equals(ipP.getProperty("innerip"))){
             //运行在服务器上
             p.setProperty("log4j.appender.logfile.File","/alidata/server/mycrawllog.log");
             p.setProperty("log4j.rootLogger","INFO,stdout,logfile");
