93633e2979901a57c8fe56db0aa39731f6ee11c0
wycm <1057160387@qq.com>
2016-05-06 20:33:47 +0800

    nothing

--- mycrawl/src/com/crawl/dao/ZhuhuDAO.java
+++ mycrawl/src/com/crawl/dao/ZhuhuDAO.java
@@ -11,7 +11,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * @author wy
+ *
  */
 public class ZhuhuDAO {
     private static Logger logger = MyLogger.getMyLogger(ZhuhuDAO.class);
