1e765ccec92b8f5d7e5a5b2e029e16cab50afddc
wycm <1057160387@qq.com>
2016-12-29 22:55:56 +0800

    update

--- src/main/java/com/crawl/proxy/ProxyPool.java
+++ src/main/java/com/crawl/proxy/ProxyPool.java
@@ -6,8 +6,14 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.DelayQueue;
 
+/**
+ * 代理池
+ */
 public class ProxyPool {
     public final static Set<String> proxySet = new HashSet<String>();
+    /**
+     * 代理池队列
+     */
     public final static DelayQueue<Proxy> queue = new DelayQueue();
 
 }
--- src/test/java/com/crawl/Test.java
+++ src/test/java/com/crawl/Test.java
@@ -1,8 +1,5 @@
 package com.crawl;
 
-/**
- * Created by yang.wang on 12/26/16.
- */
 public class Test {
     public static int j = 0;
     public static synchronized void add(){
