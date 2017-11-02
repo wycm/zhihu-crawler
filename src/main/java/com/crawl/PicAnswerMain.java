c685fa78505a5691f6e9e15801be073e49543630
wycm <1057160387@qq.com>
2017-11-02 20:49:37 +0800

    udpate

--- src/main/java/com/crawl/PicAnswerMain.java
+++ src/main/java/com/crawl/PicAnswerMain.java
@@ -1,12 +1,10 @@
 package com.crawl;
 
 
-import com.crawl.proxy.ProxyHttpClient;
 import com.crawl.zhihu.ZhiHuHttpClient;
 
 public class PicAnswerMain {
     public static void main(String[] args){
-        ProxyHttpClient.getInstance().startCrawl();
         ZhiHuHttpClient.getInstance().startCrawlAnswerPic("tang-qi-55-37");
     }
 }
